0c0b11f86eef501dcbc51b2b801ecb83f4896af5
priyuh <um.priya4@gmail.com>
2025-06-11 20:59:45 -0400

    Add TimeAvailabilityForm component and its unit test

---
+++ study-buddy-vue/src/components/TimeAvailabilityForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label for="time-select">Select Availability:</label>
+    <select id="time-select" v-model="selectedTime">
+      <option disabled value="">Please select</option>
+      <option>Weekdays</option>
+      <option>Weekends</option>
+      <option>Evenings</option>
+      <option>Anytime</option>
+    </select>
+    <button type="submit">Save</button>
+    <p v-if="message">{{ message }}</p>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'TimeAvailabilityForm',
+  data() {
+    return {
+      selectedTime: '',
+      message: ''
+    }
+  },
+  methods: {
+    handleSubmit() {
+      if (this.selectedTime) {
+        this.message = `Availability set to: ${this.selectedTime}`;
+        // You can later emit this data or save to a database
+      } else {
+        this.message = 'Please select a time.';
+      }
+    }
+  }
+}
+</script>
