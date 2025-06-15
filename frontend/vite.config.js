f8ebee7d2de767cace42248411d25ef57854b6c2
G <gesu@Gesus-MacBook-Pro.local>
2025-06-14 23:00:20 -0400

    changes to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import string from 'vite-plugin-string'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     vue(),
     string({
-      include: ['**/*.csv'],
+      include: ['**/*.csv'], // for importing CSVs
     }),
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: [],
+  },
 })
