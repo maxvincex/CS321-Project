d0bea5067523de2bbe4aca60763cdcbbebc9f3a1
priyuh <85803260+priyuh@users.noreply.github.com>
2025-06-14 22:10:33 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import string from 'vite-plugin-string'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    string({
+      include: ['**/*.csv'],
+    }),
+  ]
 })
