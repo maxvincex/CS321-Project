657518da9c3dd941c6be774a087c87c956d1d904
G <gesu@Gesus-MacBook-Pro.local>
2025-06-14 22:26:59 -0400

    updated index

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../pages/Home.vue'
-import Profile from '../pages/Profile.vue'
-import CreateAccount from '../pages/CreateAccount.vue'
-import Homepage from '../pages/Homepage.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Login from '../pages/Login.vue';
+import CreateAccount from '../pages/CreateAccount.vue';
+import Profile from '../pages/Profile.vue';
+import ChatList from '../pages/ChatList.vue';
+import Chat from '../pages/Chat.vue';
+import SearchPage from '../pages/SearchPage.vue';
+import ClassesForm from '../pages/ClassesForm.vue';
+import TimeAvailabilityForm from '../pages/TimeAvailabilityForm.vue';
 
 const routes = [
-  { path: '/', 
-    name: 'Home',
-    component: Homepage },
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/register', component: CreateAccount },
   { path: '/profile', component: Profile },
-  { path: '/create-account',
-    name: 'CreateAccount',
-    component: CreateAccount
-  }
+  { path: '/edit-classes', component: ClassesForm },
+  { path: '/edit-availability', component: TimeAvailabilityForm },
+  { path: '/chat', component: ChatList },
+  { path: '/chat/:id', component: Chat, props: true },
+  { path: '/search', component: SearchPage }
+];
 
-]
-
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
-  routes,
-})
+  routes
+});
+
+export default router;
