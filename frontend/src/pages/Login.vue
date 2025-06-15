482d00617de56560bac612d7b22f131ca716b19d
G <gesu@Gesus-MacBook-Pro.local>
2025-06-15 01:02:02 -0400

    updates for website to run - worked

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -6,14 +6,42 @@
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
         <button type="submit">Sign In</button>
-        <a href="/create-account">Create Account</a>
+        <router-link to="/register" class="text-blue-600 underline text-center block mt-4">
+          Create Account
+        </router-link>      
       </form>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+/* export default {
   name: "Home"
+}; 
+*/
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    async handleLogin() {
+      const users = JSON.parse(localStorage.getItem("users") || "[]");
+      const found = users.find(
+        (u) => u.email === this.email && u.password === this.password
+      );
+
+      if (found) {
+        localStorage.setItem("firstName", found.firstName);
+        localStorage.setItem("lastName", found.lastName);
+        this.$router.push("/profile");
+      } else {
+        alert("Invalid email or password. Try again or register.");
+      }
+    }
+  }
 };
 </script>
